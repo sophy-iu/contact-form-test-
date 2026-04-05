11fc7c35a59eea0740fc95fb42d50da0272e725f
sophy-iu <phyro8835@icloud.cpm>
2026-04-06 03:29:20 +0900

    一回目完了

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -21,73 +21,115 @@
             <h2>Contact</h2>
         </div>
 
-        <form method="POST" action="/confirm">
+        <form class="form" method="POST" action="/confirm">
             @csrf
 
             <!-- 名前 -->
-            <div class="form-group">
-                <label>お名前 <span class="required">※</span></label>
-                <div class="name-group">
-                    <input type="text" name="last_name" placeholder="例: 山田">
-                    <input type="text" name="first_name" placeholder="例: 太郎">
+            <div class="form__group">
+                <div class="form__group-title">
+                    <label>お名前 <span>※</span></label>
+                </div>
+                <div class="form__group-content">
+                    <div class="form__input--text">
+                        <input type="text" name="last_name" placeholder="例: 山田">
+                        <input type="text" name="first_name" placeholder="例: 太郎">
+                    </div>
                 </div>
             </div>
 
             <!-- 性別 -->
-            <div class="form-group">
-                <label>性別 <span class="required">※</span></label>
-                <div class="radio-group">
-                    <label><input type="radio" name="gender" value="male"> 男性</label>
-                    <label><input type="radio" name="gender" value="female"> 女性</label>
-                    <label><input type="radio" name="gender" value="other"> その他</label>
+            <div class="form__group">
+                <div class="form__group-title">
+                    <label>性別 <span>※</span></label>
+                </div>
+                <div class="form__group-content">
+                    <div class="form__input--text">
+                        <label><input type="radio" name="gender" value="male"> 男性</label>
+                        <label><input type="radio" name="gender" value="female"> 女性</label>
+                        <label><input type="radio" name="gender" value="other"> その他</label>
+                    </div>
                 </div>
             </div>
 
             <!-- メール -->
-            <div class="form-group">
-                <label>メールアドレス <span class="required">※</span></label>
-                <input type="email" name="email" placeholder="例: test@example.com">
+            <div class="form__group">
+                <div class="form__group-title">
+                    <label>メールアドレス <span>※</span></label>
+                </div>
+                <div class="form__group-content">
+                    <div class="form__input--text">
+                        <input type="email" name="email" placeholder="例: test@example.com">
+                    </div>
+                </div>
             </div>
 
             <!-- 電話番号 -->
-            <div class="form-group">
-                <label>電話番号 <span class="required">※</span></label>
-                <div class="tel-group">
-                    <input type="text" name="tel1">
-                    <span>-</span>
-                    <input type="text" name="tel2">
-                    <span>-</span>
-                    <input type="text" name="tel3">
+            <div class="form__group">
+                <div class="form__group-title">
+                    <label>電話番号 <span>※</span></label>
+                </div>
+                <div class="form__group-content">
+                    <div class="form__input--text">
+                        <input type="text" name="tel1">
+                        <span>-</span>
+                        <input type="text" name="tel2">
+                        <span>-</span>
+                        <input type="text" name="tel3">
+                    </div>
                 </div>
             </div>
 
             <!-- 住所 -->
-            <div class="form-group">
-                <label>住所 <span class="required">※</span></label>
-                <input type="text" name="address" placeholder="例: 東京都渋谷区千駄ヶ谷1-2-3">
+            <div class="form__group">
+                <div class="form__group-title">
+                    <label>住所 <span>※</span></label>
+                </div>
+                <div class="form__group-content">
+                    <div class="form__input--text">
+                        <input type="text" name="address" placeholder="例: 東京都渋谷区千駄ヶ谷1-2-3">
+                    </div>
+                </div>
             </div>
 
             <!-- 建物名 -->
-            <div class="form-group">
-                <label>建物名</label>
-                <input type="text" name="building" placeholder="例: 千駄ヶ谷マンション101">
+            <div class="form__group">
+                <div class="form__group-title">
+                    <label>建物名</label>
+                </div>
+                <div class="form__group-content">
+                    <div class="form__input--text">
+                        <input type="text" name="building" placeholder="例: 千駄ヶ谷マンション101">
+                    </div>
+                </div>
             </div>
 
             <!-- 種類 -->
-            <div class="form-group">
-                <label>お問い合わせの種類 <span class="required">※</span></label>
-                <select name="type">
-                    <option value="">選択してください</option>
-                    <option value="product">商品について</option>
-                    <option value="service">サービスについて</option>
-                    <option value="other">その他</option>
-                </select>
+            <div class="form__group">
+                <div class="form__group-title">
+                    <label>お問い合わせの種類 <span>※</span></label>
+                </div>
+                <div class="form__group-content">
+                    <div class="form__input--text">
+                        <select name="type">
+                            <option value="">選択してください</option>
+                            <option value="product">商品について</option>
+                            <option value="service">サービスについて</option>
+                            <option value="other">その他</option>
+                        </select>
+                    </div>
+                </div>
             </div>
 
             <!-- 内容 -->
-            <div class="form-group">
-                <label>お問い合わせ内容 <span class="required">※</span></label>
-                <textarea name="content" rows="5"></textarea>
+            <div class="form__group">
+                <div class="form__group-title">
+                    <label>お問い合わせ内容 <span>※</span></label>
+                </div>
+                <div class="form__group-content">
+                    <div class="form__input--textarea">
+                        <textarea name="content" rows="5"></textarea>
+                    </div>
+                </div>
             </div>
 
             <button type="submit" class="btn">確認画面</button>
